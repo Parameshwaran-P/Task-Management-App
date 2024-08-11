d0df1bb40bf7f90a1a78c1b118f6230d637d5398
paramesh-75 <parameshwaranit22@gmail.com>
2024-08-11 14:19:49 +0530

    add changes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const sequelize = require('./config/dbConfig');
+const sequelize = require('');
 
 // Import routes
 const authRoutes = require('./routes/api/authRoutes');
--- backend/routes/api/authRoutes.js
+++ backend/routes/api/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const pool = require('../db');
+const pool = require('../../config/dbConfig');
 
 // Signup
 router.post('/signup', async (req, res) => {
